4f6567ee747abeba2c8b7377d0cc210bed46ed8f
Manuel Osorio Wallace <mosoriow@gmail.com>
2020-11-04 16:20:01 +0100

    2020-11-04 uppgifter

---
+++ 03. PersonInfo/PersonInfo/Citizen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+    }
+}
---
+++ Morgan and a String/Morgan and a String/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace Morgan_and_a_String
+{
+    class Program
+    {
+        static string morganAndString(string a, string b)
+        {
+            string result = "";
+            
+            char[] av = a.ToCharArray();
+            char[] bv = b.ToCharArray();
+
+            int avi = 0;
+            int bvi = 0;
+
+            while ((avi < av.Length) || (bvi < bv.Length))
+            {
+                if ((avi < av.Length) && (bvi < bv.Length))
+                {
+                    if(av[avi]<= bv[bvi])
+                    {
+                        //result = result + av[avi];
+                        result.Concat(av[avi].ToString());
+                        avi++;
+                    }
+                    else
+                    {
+                        //result = result + bv[bvi];
+                        result.Concat(bv[bvi].ToString());
+                        bvi++;
+                    }
+                }
+                else
+                {
+                    if (avi < av.Length)
+                    {
+                        //result = result + av[avi];
+                        result.Concat(av[avi].ToString());
+                        avi++;
+                    }
+                    else if (bvi < bv.Length)
+                    {
+                        //result = result + bv[bvi];
+                        result.Concat(bv[bvi].ToString());
+                        bvi++;
+                    }
+                }
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                string a = Console.ReadLine();
+                string b = Console.ReadLine();
+
+                string result = morganAndString(a, b);
+
+                Console.WriteLine("Output:");
+                Console.WriteLine(result);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
